16a1e8070a15b93f40de5a27b282269cfcbfee1a
Cristi Bârlădeanu <cristi@non.ro>
2015-05-04 11:29:05 +0300

    Added item structure and random background color

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,12 @@ $(function(){
         $( this ).addClass('active');
       });
     });
+
+  });
+
+   $('.item').each(function () {
+      var hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
+      $(this).css("background-color", hue);
   });
 
 });
