e5a8f70d4554714d04207498ae18b7fcc5177f9e
Maria Roman <mariaroman@Marias-MacBook-Air.local>
2015-09-28 15:33:33 +0300

    Modified the select component

--- app/public/js/scripts.js
+++ app/public/js/scripts.js
@@ -1,39 +1,38 @@
 $(function () {
-
     $('#select-category').multiselect();
 
-    var $container = $('.isotope > .row').imagesLoaded(function () {
-        $container.isotope({
-            itemSelector: '.item',
-            layoutMode: 'masonry',
-            getSortData: {
-                interest: '[data-interest]',
-                members: function(item) {
-                    return parseInt($(item).data('members'));
-                },
-                alphabetical: 'h3'
-            }
-        });
-
-        $('.filters').on('click', 'button', function () {
-            var filterValue = $(this).attr('data-filter');
-            $container.isotope({ filter: filterValue });
-        });
-
-        $('.sorts').on('click', 'button', function () {
-            var sortValue = $(this).attr('data-sort');
-            sortValue = sortValue.split(',');
-            $container.isotope({ sortBy: sortValue, sortAscending: sortValue != 'members' });
-        });
-
-        $('.filters, .sorts').each(function (i, buttonGroup) {
-            var $buttonGroup = $(buttonGroup);
-            $buttonGroup.on('click', 'button', function () {
-                $buttonGroup.find('.active').removeClass('active');
-                $(this).addClass('active');
-            });
-        });
-    });
+    //var $container = $('.isotope > .row').imagesLoaded(function () {
+    //    $container.isotope({
+    //        itemSelector: '.item',
+    //        layoutMode: 'masonry',
+    //        getSortData: {
+    //            interest: '[data-interest]',
+    //            members: function(item) {
+    //                return parseInt($(item).data('members'));
+    //            },
+    //            alphabetical: 'h3'
+    //        }
+    //    });
+    //
+    //    $('.filters').on('click', 'button', function () {
+    //        var filterValue = $(this).attr('data-filter');
+    //        $container.isotope({ filter: filterValue });
+    //    });
+    //
+    //    $('.sorts').on('click', 'button', function () {
+    //        var sortValue = $(this).attr('data-sort');
+    //        sortValue = sortValue.split(',');
+    //        $container.isotope({ sortBy: sortValue, sortAscending: sortValue != 'members' });
+    //    });
+    //
+    //    $('.filters, .sorts').each(function (i, buttonGroup) {
+    //        var $buttonGroup = $(buttonGroup);
+    //        $buttonGroup.on('click', 'button', function () {
+    //            $buttonGroup.find('.active').removeClass('active');
+    //            $(this).addClass('active');
+    //        });
+    //    });
+    //});
 
     $('#myModal').modal({ show: false});
 
